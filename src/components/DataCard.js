d12186995a6c451eb967a4ccd9fb97d62dfc7551
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2021-04-26 19:57:46 +0530

    CSS DONE

--- src/components/DataCard.js
+++ src/components/DataCard.js
@@ -1,16 +1,18 @@
 import React from "react";
-
+import "../style/data-card.css";
 const DataCard = ({ city, distributor, address, number, status }) => {
   return (
-    <a href={`tel:${number}`}>
-      <div className={status ? "data-card" : "data-card-red"}>
-        <h1>{city}</h1>
-        <h2>{distributor}</h2>
-        <h3>{address}</h3>
-        <h4>{number}</h4>
-        <h5>Click to call</h5>
-      </div>
-    </a>
+    <div className="card">
+      <a className="card-a" href={"tel:" + { number }}>
+        <div className={status ? "data-card" : "data-card-red"}>
+          <h1>{city}</h1>
+          <h2>{distributor}</h2>
+          <h3>{address}</h3>
+          <h4 className="number">{number}</h4>
+          <h5 className="call">Click to call</h5>
+        </div>
+      </a>
+    </div>
   );
 };
 
