b40674fc8540f49b016482dff4fe964f8fb49eab
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2021-04-28 14:50:42 +0530

    Tel Done

--- src/components/DataCard.js
+++ src/components/DataCard.js
@@ -5,16 +5,16 @@ const DataCard = ({ city, distributor, address, number, status, comment }) => {
     <div className="card_main">
       <div className="card">
         <div className="dataCard_main">
-          <a href={"tel:" + { number }}>
-          <div className="card-content">
-          <h1 className="city">{city}</h1>
-          <h2 className="distributor">{distributor}</h2>
-          <h3 className="address">{address}</h3>
-          <h4 className="number">{number}</h4>
-          <div className={status ? "data-card" : "data-card-red"}>
-            <h3 className="comment">{comment}</h3>
-            </div>
-            <h5 className="call">Click to call</h5>
+          <a href={`tel:${number}`}>
+            <div className="card-content">
+              <h1 className="city">{city}</h1>
+              <h2 className="distributor">{distributor}</h2>
+              <h3 className="address">{address}</h3>
+              <h4 className="number">{number}</h4>
+              <div className={status ? "data-card" : "data-card-red"}>
+                <h3 className="comment">{comment}</h3>
+              </div>
+              <h5 className="call">Click to call</h5>
             </div>
           </a>
         </div>
