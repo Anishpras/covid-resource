f9efea75131dbbb124540fc736ccfa975c350b66
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2021-04-28 19:04:50 +0530

    Update 3.0

--- src/data/Every.js
+++ src/data/Every.js
@@ -2,7 +2,7 @@ const EveryData = [
   {
     city: "All Bihar Cities",
     distibutor_name: "Deepak Kumar",
-    helpline: "8083333924, 7070832856",
+    helpline: "8083333924",
     address: " ",
     status: true,
     comment: "Every Need",
