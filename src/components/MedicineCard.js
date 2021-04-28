e4e468fe47ae29cee939e6e70cade3434d1da71d
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2021-04-28 15:50:28 +0530

    Medicine Card Done

--- src/components/MedicineCard.js
+++ src/components/MedicineCard.js
@@ -3,15 +3,17 @@ import { Link } from "react-router-dom";
 import "../Styles/MedicineCard.css";
 const MedicineCard = ({ name, link }) => {
   return (
-    <div className="medicineCard_main">
-      <div className="MedicineCard_content">
-        <Link to={link}>
-          <div className="medicine-card">
-            <h1>{name}</h1>
-          </div>
-        </Link>
+    <Link to={link}>
+      <div className="medicineCard_main">
+        <div className="MedicineCard_content">
+          <Link to={link}>
+            <div className="medicine-card">
+              <h1>{name}</h1>
+            </div>
+          </Link>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
