3d8d40d5befacbd02c19f30e51d7477f34f465e3
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2021-04-29 13:07:42 +0530

    Covidkit2021

--- src/routes/ResourcePage.jsx
+++ src/routes/ResourcePage.jsx
@@ -41,13 +41,22 @@ const ResourcePage = () => {
   return (
     <div className="resource-page">
       <div className="resourcePage_content">
-      <h4 className="resourcepage-h4"style={{ color: "black" }}>
-        NOTE-{" "}
-        <strong style={{ color: "black" }}>
-          If any link or number is not working inform us at
-          biharcovid428@gmail.com mob: 9162248329
-        </strong>
-      </h4>
+        <h4 className="resourcepage-h4" style={{ color: "black" }}>
+          NOTE-{" "}
+          <strong style={{ color: "black" }}>
+            If any link or number is not working inform us at
+            biharcovid428@gmail.com mob: 9162248329
+          </strong>
+        </h4>
+        <h2>
+          Follow for more details:{" "}
+          <a
+            href="https://instagram.com/covidkit2021?igshid=1rdbpdbiuyky9"
+            target="_blank"
+            rel="noreferrer">
+            @covidkit2021
+          </a>
+        </h2>
         <div className="resourcePage_main">
           <div className="resourcePage_Card">
             {resources.map((resource) => {
